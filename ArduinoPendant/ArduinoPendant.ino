378fc1db94e9cc95c98a40626fd3c2d524105dec
Tim Holum <tholum@couleetechlink.com>
2015-07-22 13:48:06 -0500

    First Bit of code

--- ArduinoPendant/ArduinoPendant.ino
+++ ArduinoPendant/ArduinoPendant.ino
@@ -1,14 +1,42 @@
+#include "U8glib.h"
+U8GLIB_ST7920_128X64_1X u8g(17, 16, 15);	// SPI Com: SCK = en = 18, MOSI = rw = 16, CS = di = 17
 
+void draw(void) {
+  // graphic commands to redraw the complete screen should be placed here  
+  u8g.setFont(u8g_font_unifont);
+  //u8g.setFont(u8g_font_osb21);
+  u8g.drawStr( 0, 22, "Hello World!");
+}
 
-void setup(){
-Serial.begin(115200); 
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
+void setup(void) {
+  // flip screen, if required
+  // u8g.setRot180();
+  
+  // set SPI backup if required
+  //u8g.setHardwareBackup(u8g_backup_avr_spi);
+
+  // assign default color value
+  if ( u8g.getMode() == U8G_MODE_R3G3B2 ) {
+    u8g.setColorIndex(255);     // white
+  }
+  else if ( u8g.getMode() == U8G_MODE_GRAY2BIT ) {
+    u8g.setColorIndex(3);         // max intensity
+  }
+  else if ( u8g.getMode() == U8G_MODE_BW ) {
+    u8g.setColorIndex(1);         // pixel on
+  }
+  else if ( u8g.getMode() == U8G_MODE_HICOLOR ) {
+    u8g.setHiColorByRGB(255,255,255);
   }
 }
 
-void loop(){
-  if (Serial.available() > 0) {
-    
-  }
+void loop(void) {
+  // picture loop
+  u8g.firstPage();  
+  do {
+    draw();
+  } while( u8g.nextPage() );
+  
+  // rebuild the picture after some delay
+  delay(50);
 }
---
+++ ArduinoPendant/README.md
@@ -0,0 +1,3 @@
+This Project requires Universal 8bit Graphics Library, http://code.google.com/p/u8glib/ To be installed
+
+Currently I am testing on an arduino teency++ 2.0, But the final hardware is likely going to be a 32u4 chip
